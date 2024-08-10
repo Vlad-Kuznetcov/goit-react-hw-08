6ca1f9b4649b9926cc2e62003fd43c25ac72f339
Vlad <vladonik.909@gmail.com>
2024-08-10 17:59:44 +0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,28 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import { nanoid } from "nanoid";
 
 function App() {
-  // const [contacts, setContacts] = useState([
-  //   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  //   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  //   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  //   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  // ]);
+  // function App() {
+  //   const [catch, setContacts] = useState(() => {
+  //     try {
+  //       const storedContacts = window.localStorage.getItem("contactList");
+  //       return storedContacts ? JSON.parse(storedContacts) : initialValues;
+  //     } catch (e) {
+  //       console.error("Error parsing localStorage data:", e);
+  //       return initialValues;
+  //     }
+  //   });
 
-  const [contacts, setContacts] = useState(() =>
-    JSON.parse(
-      window.localStorage.getItem("contacts") ?? [
-        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-      ]
-    )
-  );
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = JSON.parse(window.localStorage.getItem("contacts"));
+    return savedContacts !== null && savedContacts.length !== 0
+      ? savedContacts
+      : [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
+  });
 
   const [searchName, setSearchName] = useState("");
 
